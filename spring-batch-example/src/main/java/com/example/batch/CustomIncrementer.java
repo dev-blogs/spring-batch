eb1a5783236051e850a43a1ca2ca353a21586dc2
zheka <user@dev-blogs.com>
2020-05-11 19:29:46 +0300

    added additional tests

--- spring-batch-example/src/main/java/com/example/batch/CustomIncrementer.java
+++ spring-batch-example/src/main/java/com/example/batch/CustomIncrementer.java
@@ -8,9 +8,9 @@ public class CustomIncrementer implements JobParametersIncrementer {
 	@Override
 	public JobParameters getNext(JobParameters parameters) {
 		return new JobParametersBuilder()
-				.addString("inputResource", "classpath:/input/products.zip")
+				.addString("inputResource", "classpath:/input/products2.zip")
 				.addString("targetDirectory", "./target/importproductsbatch/")
-				.addString("targetFile", "products.txt")
+				.addString("targetFile", "products2.txt")
 				.addLong("timestamp", System.currentTimeMillis())
 				.toJobParameters();
 	}
