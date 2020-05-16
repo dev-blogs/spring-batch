5ed4091711c03a7164942481d810f5d161637c82
zheka <user@dev-blogs.com>
2020-05-16 19:02:45 +0300

    changed job-repository from persistante to in-memory

--- spring-batch-example/src/test/java/com/example/ProductStepTest.java
+++ spring-batch-example/src/test/java/com/example/ProductStepTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { 
 		"/spring/import-products-job-context.xml", 
-		"/spring/infrustructure-context.xml" 
 	})
 public class ProductStepTest {
 	@Autowired
