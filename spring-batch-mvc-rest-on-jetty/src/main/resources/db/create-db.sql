9404452d35b056395369ca7522f9fd2608c52d44
zheka <user@dev-blogs.com>
2020-05-16 19:17:34 +0300

    Removed unsued projects for this branch

--- spring-batch-mvc-rest-on-jetty/src/main/resources/db/create-db.sql
+++
@@ -1,6 +0,0 @@
-create table products (
-	id varchar(10),
-	name varchar(255),
-	description varchar(255),
-	price decimal(19,2)
-);
--- spring-batch-on-jetty/src/main/java/com/devblogs/service/BatchJobService.java
+++
@@ -1,5 +0,0 @@
-package com.devblogs.service;
-
-public interface BatchJobService {
-	String lanchJob() throws Exception;
-}
