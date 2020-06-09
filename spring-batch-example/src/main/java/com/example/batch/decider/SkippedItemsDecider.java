9350fd6a410e0fb1fe839f84d0be313396f28ac1
zheka <user@dev-blogs.com>
2020-06-09 15:07:05 +0300

    Added dicider

---
+++ spring-batch-example/src/main/java/com/example/batch/decider/SkippedItemsDecider.java
@@ -0,0 +1,19 @@
+package com.example.batch.decider;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+public class SkippedItemsDecider implements JobExecutionDecider {
+	@Override
+	public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+		if (!ExitStatus.FAILED.equals(stepExecution.getExitStatus())
+				&& stepExecution.getSkipCount() > 0) {
+			return new FlowExecutionStatus("COMPLETED WITH SKIPS");
+		} else {
+			return new FlowExecutionStatus(jobExecution.getExitStatus().toString());
+		}
+	}
+}
