760a4a0029995f88d6b3cbe7db02589ef7774364
zheka <user@dev-blogs.com>
2020-10-21 19:44:02 +0300

    Changed setting job execution context by scope step

--- spring-batch-example/src/main/java/com/example/batch/tasklet/TrackImportFromJobContextTasklet.java
+++ spring-batch-example/src/main/java/com/example/batch/tasklet/TrackImportFromJobContextTasklet.java
@@ -3,14 +3,17 @@ package com.example.batch.tasklet;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 
 public class TrackImportFromJobContextTasklet implements Tasklet {
+	private String importId;
+	
+	public void setImportId(String importId) {
+		this.importId = importId;
+	}
+
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
-		String importId = jobExecutionContext.getString("importId");
 		System.out.println("importId: " + importId);
 		return RepeatStatus.FINISHED;
 	}
