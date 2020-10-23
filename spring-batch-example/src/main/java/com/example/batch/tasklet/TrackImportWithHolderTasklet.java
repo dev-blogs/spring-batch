b4740daf6ff37a2048d1266cdd0f42d5eda8d7f9
zheka <user@dev-blogs.com>
2020-10-23 22:02:55 +0300

    Added holder and late binding inject bean

--- spring-batch-example/src/main/java/com/example/batch/tasklet/TrackImportWithHolderTasklet.java
+++ spring-batch-example/src/main/java/com/example/batch/tasklet/TrackImportWithHolderTasklet.java
@@ -4,18 +4,17 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import com.example.batch.data.ImportMetadataHolder;
 
 public class TrackImportWithHolderTasklet implements Tasklet {
-	private ImportMetadataHolder importMetadataHolder;
+	private String importId;
 	
-	public void setImportMetadataHolder(ImportMetadataHolder importMetadataHolder) {
-		this.importMetadataHolder = importMetadataHolder;
+	public void setImportId(String importId) {
+		this.importId = importId;
 	}
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		System.out.println("importId: " + importMetadataHolder.get().getImportId());
+		System.out.println("importId: " + importId);
 		return RepeatStatus.FINISHED;
 	}
 }
