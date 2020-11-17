e4148f3aa1d6ffa2af31057f17e3de9db81f2812
zheka <user@dev-blogs.com>
2020-11-17 22:28:32 +0300

    Code development. Changed launch method from GET to PUT method request
    + Changed incoming parameters, added imortId as request parameter and
    content as request body

--- spring-batch-mvc/src/main/java/com/example/web/controller/JobLauncherController.java
+++ spring-batch-mvc/src/main/java/com/example/web/controller/JobLauncherController.java
@@ -2,13 +2,16 @@ package com.example.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 import com.example.repository.jdbc.ProductImportRepository;
 
 @Controller
@@ -16,14 +19,15 @@ public class JobLauncherController {
 	@Autowired
 	private ProductImportRepository productImportRepository;
 	
-	@RequestMapping(value = "launch", method = RequestMethod.GET)
+	@RequestMapping(value = "/product-imports/{importId}", method = RequestMethod.PUT)
 	@ResponseStatus(HttpStatus.ACCEPTED)
-	public void launch(@RequestParam String name, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public void launch(@PathVariable String importId, @RequestBody String content, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		response.setContentType("text/html");
 		response.setStatus(HttpServletResponse.SC_OK);
-		response.getWriter().println("echo: " + name + "<br>");
+		response.getWriter().println("echo importId: " + importId + "<br>");
+		response.getWriter().println("echo content: " + content + "<br>");
 		
-		productImportRepository.createProductImport(name);
+		productImportRepository.createProductImport(importId);
 		
 		response.getWriter().println("session=" + request.getSession(true).getId());
 	}
--- spring-batch-mvc/src/test/resources/h2/schema.sql
+++ spring-batch-mvc/src/test/resources/h2/schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE product_import (
 	import_id	VARCHAR2(15),
+	content		VARCHAR2(4000),
 	creation_date	DATE
 );
