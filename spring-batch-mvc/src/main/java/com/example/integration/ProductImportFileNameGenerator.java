bdc4015f2ae37e059af1399e9d64a0619fc3e7b1
zheka <user@dev-blogs.com>
2020-11-20 20:07:44 +0300

    Code development. Added file-writing channel
    + Added spring-integration-file artifact to pom.xml
    + Added schema/integration/file schema to applicationContext.xml
    + Added outbound-channel-adapter compoment to applicationContext.xml
    + Added ProductImportFileNameGenerator bean

---
+++ spring-batch-mvc/src/main/java/com/example/integration/ProductImportFileNameGenerator.java
@@ -0,0 +1,16 @@
+package com.example.integration;
+
+import org.springframework.integration.Message;
+import org.springframework.integration.file.FileNameGenerator;
+import org.springframework.util.Assert;
+
+public class ProductImportFileNameGenerator implements FileNameGenerator {
+	@Override
+	public String generateFileName(Message<?> message) {
+		Assert.notNull(message.getPayload());
+		Assert.isInstanceOf(String.class, message.getPayload());
+		
+		String payload = (String) message.getPayload();
+		return payload.getClass() + ".xml";
+	}
+}
