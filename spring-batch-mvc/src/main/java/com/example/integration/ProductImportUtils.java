54529117bdad842349752aaf3e0203a552479613
zheka <user@dev-blogs.com>
2020-11-25 23:47:31 +0300

    Code development. Added commons-io artifactory and added xml parser to ProductImportUtils class

--- spring-batch-mvc/src/main/java/com/example/integration/ProductImportUtils.java
+++ spring-batch-mvc/src/main/java/com/example/integration/ProductImportUtils.java
@@ -1,7 +1,17 @@
 package com.example.integration;
 
+import java.io.StringReader;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import com.example.model.Products;
+
 public class ProductImportUtils {
-	public static String extractImportId(String content) {
-		return "filename.xml";
+	public static String extractImportId(String content) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Products.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		StringReader stringReader = new StringReader(content);
+		Products customer = (Products) jaxbUnmarshaller.unmarshal(stringReader);
+		return customer.getImportId() + ".xml";
 	}
 }
